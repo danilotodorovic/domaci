20856ac5f556f80fcddeda167d2629feee369af1
Danilo <danilotodorqvic@gmail.com>
2020-02-13 11:34:43 +0100

    nedelja3 ispravljeno

--- Nedelja3/zadatak1.js
+++ Nedelja3/zadatak1.js
@@ -1,18 +1,18 @@
-function getNajveciBroj(broj1,broj2,broj3){
-    let rezultat = Math.max(broj1,broj2,broj3)
+function getNajveciBroj(broj1, broj2, broj3) {
+    let rezultat = Math.max(broj1, broj2, broj3)
     return `Najveci broj je ${rezultat}`;
 }
-let info = getNajveciBroj (2,30,231)
-console.log(info);
+//let info = getNajveciBroj(2, 30, 231)
+//console.log(info);
 
-function maksimum(br1,br2,br3){
-    maksimum=br1;
-    if(maksimum<br2){
-        maksimum=br2;
-    }else if(maksimum<br3){
-        maksimum=br3;
+function maksimum(br1, br2, br3) {
+    maksimum = br1;
+    if (maksimum < br2) {
+        maksimum = br2;
+    } if (maksimum < br3) {
+        maksimum = br3;
     }
     return maksimum;
 }
-let max = maksimum(2,5,3)
+let max = maksimum(5, 11, 13)
 console.log(max);
